37b9689a75dee3a0f1ac1e48e50c186e41669b3b
Tanner Kirkpatrick <twk4491@gmail.com>
2021-03-04 13:22:14 -0500

    logs component built out

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import SearchBar from "./components/layout/SearchBar";
+import Logs from "./components/logs/Logs";
 import "./index.css";
 
 const App = () => {
@@ -11,6 +12,9 @@ const App = () => {
   return (
     <>
       <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
     </>
   );
 };
