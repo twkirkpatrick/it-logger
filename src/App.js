c848cb4406f968756708ef1757e1e3f5e03d5b20
Tanner Kirkpatrick <twk4491@gmail.com>
2021-03-04 13:55:47 -0500

    floating action button built out

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import SearchBar from "./components/layout/SearchBar";
 import Logs from "./components/logs/Logs";
+import AddBtn from "./components/layout/AddBtn";
 import "./index.css";
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
     <>
       <SearchBar />
       <div className="container">
+        <AddBtn />
         <Logs />
       </div>
     </>
