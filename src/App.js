284fadf641013eaa1ff0d39e2808d65b315f1c53
Tanner Kirkpatrick <twk4491@gmail.com>
2021-03-04 12:43:08 -0500

    navbar brought in from materialize

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { useEffect } from "react";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+import SearchBar from "./components/layout/SearchBar";
 import "./index.css";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  useEffect(() => {
+    M.AutoInit();
+  });
+  return (
+    <>
+      <SearchBar />
+    </>
+  );
+};
 
 export default App;
