9d68c4a182b66a486d29410cd3a8fbb2642b54ea
Tanner Kirkpatrick <twk4491@gmail.com>
2021-03-05 20:19:35 -0500

    log modal built out

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import M from "materialize-css/dist/js/materialize.min.js";
 import SearchBar from "./components/layout/SearchBar";
 import Logs from "./components/logs/Logs";
 import AddBtn from "./components/layout/AddBtn";
+import AddLogModal from "./components/logs/AddLogModal";
 import "./index.css";
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
       <SearchBar />
       <div className="container">
         <AddBtn />
+        <AddLogModal />
         <Logs />
       </div>
     </>
