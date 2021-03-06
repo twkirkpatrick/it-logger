8e6bc90b3e082f48b16784f964bb012eec85a491
Tanner Kirkpatrick <twk4491@gmail.com>
2021-03-05 20:45:23 -0500

    add tech modal built out

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import SearchBar from "./components/layout/SearchBar";
 import Logs from "./components/logs/Logs";
 import AddBtn from "./components/layout/AddBtn";
 import AddLogModal from "./components/logs/AddLogModal";
+import EditLogModal from "./components/logs/EditLogModal";
+import AddTechModal from "./components/techs/AddTechModal";
 import "./index.css";
 
 const App = () => {
@@ -17,6 +19,8 @@ const App = () => {
       <div className="container">
         <AddBtn />
         <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
         <Logs />
       </div>
     </>
