105573dca02bd865b12193df6d9357feda7432ca
Tanner Kirkpatrick <twk4491@gmail.com>
2021-03-06 12:54:12 -0500

    root reducer set up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
+import store from "./store";
+import { Provider } from "react-redux";
 import SearchBar from "./components/layout/SearchBar";
 import Logs from "./components/logs/Logs";
 import AddBtn from "./components/layout/AddBtn";
@@ -15,17 +17,19 @@ const App = () => {
     M.AutoInit();
   });
   return (
-    <>
-      <SearchBar />
-      <div className="container">
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTechModal />
-        <TechListModal />
-        <Logs />
-      </div>
-    </>
+    <Provider store={store}>
+      <>
+        <SearchBar />
+        <div className="container">
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </>
+    </Provider>
   );
 };
 
