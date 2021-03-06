ba1152c2b0507901a24778d7d008a870cab5a5a6
Tanner Kirkpatrick <twk4491@gmail.com>
2021-03-06 14:53:42 -0500

    addLog action and reducer built out

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
+import { GET_LOGS, ADD_LOG, SET_LOADING, LOGS_ERROR } from "./types";
 
 export const getLogs = () => async (dispatch) => {
   try {
@@ -8,6 +8,32 @@ export const getLogs = () => async (dispatch) => {
 
     dispatch({
       type: GET_LOGS,
+      payload: data
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data
+    });
+  }
+};
+
+export const addLog = (log) => async (dispatch) => {
+  try {
+    setLoading();
+
+    const res = await fetch("/logs", {
+      method: "POST",
+      body: JSON.stringify(log),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+
+    const data = await res.json();
+
+    dispatch({
+      type: ADD_LOG,
       payload: data
     });
   } catch (err) {
