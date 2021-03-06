2ca9389d27d346082f5931f25984f6e5a084258c
Tanner Kirkpatrick <twk4491@gmail.com>
2021-03-06 15:01:21 -0500

    AddLogModal connected to Redux state

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
 import M from "materialize-css/dist/js/materialize.min.js";
+import { addLog } from "../../actions/logActions";
 
-const AddLogModal = () => {
+const AddLogModal = ({ addLog }) => {
   const [message, setMessage] = useState("");
   const [attention, setAttention] = useState(false);
   const [tech, setTech] = useState("");
@@ -10,7 +12,16 @@ const AddLogModal = () => {
     if (message === "" || tech === "") {
       M.toast({ html: "Please enter a message and tech" });
     } else {
-      console.log(message, tech, attention);
+      const newLog = {
+        message,
+        attention,
+        tech,
+        date: new Date()
+      };
+
+      addLog(newLog);
+
+      M.toast({ html: `Log added by ${tech}` });
       setMessage("");
       setTech("");
       setAttention(false);
@@ -85,4 +96,4 @@ const modalStyle = {
   height: "75%"
 };
 
-export default AddLogModal;
+export default connect(null, { addLog })(AddLogModal);
