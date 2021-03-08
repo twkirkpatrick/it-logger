814caf4e8ee50c437b85475c6f4a088be81bb01d
Tanner Kirkpatrick <twk4491@gmail.com>
2021-03-08 18:03:28 -0500

    TechSelectOptions component added for dynamic tech selection

--- src/components/logs/AddLogModal.js
+++ src/components/logs/AddLogModal.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { connect } from "react-redux";
 import M from "materialize-css/dist/js/materialize.min.js";
 import { addLog } from "../../actions/logActions";
+import TechSelectOptions from "../techs/TechSelectOptions";
 
 const AddLogModal = ({ addLog }) => {
   const [message, setMessage] = useState("");
@@ -55,9 +56,7 @@ const AddLogModal = ({ addLog }) => {
               <option value="" disabled>
                 Select Technician
               </option>
-              <option value="John Doe">John Doe</option>
-              <option value="Sam Smith">Sam Smith</option>
-              <option value="Sara Wilson">Sara Wilson</option>
+              <TechSelectOptions />
             </select>
           </div>
         </div>
